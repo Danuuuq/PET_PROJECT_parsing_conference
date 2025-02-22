789f7ea12973fd89aed05744b34a1fa2b197c43e
Daniil Tyunkov <daniil@tyunkov.ru>
2025-02-22 16:52:33 +0300

    clear

--- parser_conference/spiders/conference.py
+++ parser_conference/spiders/conference.py
@@ -9,7 +9,6 @@ class ConferenceSpider(scrapy.Spider):
     name = "conference"
     current_time = datetime.now().strftime('%Y-%m-%d')
     url = f"""http://vcs.gazsvyaz.gazprom.ru/issues?c%5B%5D=due_date&c%5B%5D=cf_6&c%5B%5D=cf_7&c%5B%5D=subject&c%5B%5D=cf_5&c%5B%5D=cf_3&c%5B%5D=description&f%5B%5D=status_id&f%5B%5D=due_date&f%5B%5D=&group_by=&op%5Bdue_date%5D=%3D&op%5Bstatus_id%5D=o&per_page=50&set_filter=1&utf8=%E2%9C%93&v%5Bdue_date%5D%5B%5D={current_time}"""
-    # url = f"""http://vcs.gazsvyaz.gazprom.ru/issues?c%5B%5D=due_date&c%5B%5D=cf_6&c%5B%5D=cf_7&c%5B%5D=subject&c%5B%5D=cf_5&c%5B%5D=description&f%5B%5D=status_id&f%5B%5D=due_date&f%5B%5D=&group_by=&op%5Bdue_date%5D=%3D&op%5Bstatus_id%5D=o&per_page=50&set_filter=1&utf8=%E2%9C%93&v%5Bdue_date%5D%5B%5D={current_time}"""
     allowed_domains = ["vcs.gazsvyaz.gazprom.ru"]
     start_urls = [url]
 
@@ -23,17 +22,17 @@ class ConferenceSpider(scrapy.Spider):
                 conf['start_time'] = row.css('td.cf_6::text').get()
                 conf['end_time'] = row.css('td.cf_7::text').get()
                 conf['organizer'] = row.css('td.cf_5::text').get()
-                # breakpoint()
                 if re.search(r'^(?!.*[Фф]илиал).*?\s[Ии]нвест[»"]$', conf['organizer']):
                     conf['manager'] = row.css('td.cf_3::text').get()
+                else:
+                    conf['manager'] = None
             else:
                 room = row.css('li::text').re(r'^(?!.*[Фф]илиал).*?\s[Ии]нвест[»"]\s*\(к.*')
                 comment = row.css('strong:contains("По")::text').get()
-                after_call = row.css('em::text').re(r'^.*?\s[Ии]нвест»\s*\(к.*')
+                after_call = row.css('em::text').re_first(r'^.*?\s[Ии]нвест»\s*\([к3]*')
                 if len(room) == 0:
                     conf = {}
                 else:
-                    # breakpoint()
                     conf['room'] = room
                     conf['comment'] = comment
                     conf['after_call'] = after_call
--- parser_conference/utils.py
+++ parser_conference/utils.py
@@ -6,25 +6,92 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 from_mail = 'conference@invest.gazprom.ru'
-# to_mail = ('epetukhova@invest.gazprom.ru', 'vvmarchenko@invest.gazprom.ru', 'dtyunkov@invest.gazprom.ru')
-to_mail =  'dtyunkov@invest.gazprom.ru'
+# to_mail =  'dtyunkov@invest.gazprom.ru'
+# to_mail =  'dtyunkov@invest.gazprom.ru,drezinkov@invest.gazprom.ru'
+to_mail = 'epetukhova@invest.gazprom.ru,vvmarchenko@invest.gazprom.ru,dtyunkov@invest.gazprom.ru'
 server_email = 'mail.vk.giz.int'
 
 
-def format_attach(data):
-    return f'Новые совещания в расписании: {data}'
+def format_attach(current_time, new_conf, today_conf):
+    message = """
+    <html>
+     <head></head>
+      <body>
+        <h2>Новые совещания после: {current_time}</h2>
+          <table>
+            <tr>
+              <td>Время начала</td>
+              <td>|Время окончания</td>
+              <td>|Студии</td>
+              <td>|Комментарий</td>
+              <td>|Организатор</td>
+              <td>|Ответственный</td>
+            </tr>
+    """.format(current_time=current_time)
+    for conf in new_conf:
+        message += """<td>{start}</td>
+                    <td>|{end}</td>
+                    <td>|{room}</td>
+                    <td>|{comment}</td>
+                    <td>|{org}</td>
+                    <td>|{man}</td></tr>""".format(
+                        # date=conf['date'],
+                        start=conf['start_time'],
+                        end=conf['end_time'],
+                        room=', '.join(conf['room']),
+                        comment=conf['comment'],
+                        org=conf['organizer'],
+                        man=conf['manager']
+                    )
+    message += "</table>"
+    if len(today_conf.all()) != 0:
+        message += """
+            <br><br><br>
+            <h2>Все совещания на сегодня</h2>
+              <table>
+                <tr>
+                  <td>Время начала</td>
+                  <td>|Время окончания</td>
+                  <td>|Студии</td>
+                  <td>|Комментарий</td>
+                  <td>|Организатор</td>
+                  <td>|Ответственный</td>
+                </tr>
+        """
+        for conf in today_conf:
+            conf = conf.__dict__
+            message += """<td>{start}</td>
+                        <td>|{end}</td>
+                        <td>|{room}</td>
+                        <td>|{comment}</td>
+                        <td>|{org}</td>
+                        <td>|{man}</td></tr>""".format(
+                            # date=conf['date'],
+                            start=conf['start'].strftime('%H:%M'),
+                            end=conf['end'].strftime('%H:%M'),
+                            room=conf['rooms'],
+                            comment=conf['comment'],
+                            org=conf['organizer'],
+                            man=conf['manager']
+                        )
+    message += "</table>"
+    message += """
+     </body>
+    </html>
+    """
+    return message
 
 
-def send_email(data):
-    current_day = datetime.now().strftime('%Y-%m-%d %H:%M')
-    subject = f'Совещания на {current_day}'
+def send_email(new_conf, today_conf):
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M')
+    subject = f'Новые совещания на {current_time}'
     msg = MIMEMultipart()
     msg['From'] = from_mail
     msg['To'] = to_mail
     msg['Subject'] = subject
-    msg.attach(MIMEText(format_attach(data), 'html', 'utf-8'))
+    msg.attach(MIMEText(format_attach(current_time, new_conf, today_conf), 'html', 'utf-8'))
     smtp = smtplib.SMTP(server_email)
-    smtp.sendmail(from_mail, to_mail, msg.as_string())
+    smtp.sendmail(from_mail, to_mail.split(','), msg.as_string())
     smtp.quit()
 
 
